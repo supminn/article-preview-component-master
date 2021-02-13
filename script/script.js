15b667b15b407e91319d4bb5ab95f1fa024dad3c
supminn <30731236+supminn@users.noreply.github.com>
2021-02-13 23:20:49 +0530

    feat: added triangle to active state

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 const btnClick = document.querySelector("#icon-share");
 const shareContainer = document.querySelector(".click-data");
 const btnActive = document.querySelector("#icon-share-active");
+const btnToggle = document.querySelector(".icon-btn");
+const btnActiveToggle = document.querySelector(".icon-btn-active");
 
 btnClick.addEventListener("click",() => {
     shareContainer.classList.remove("hidden");
+    btnToggle.classList.add("hidden");
+    btnActiveToggle.classList.remove("hidden");
 })
 
 btnActive.addEventListener("click",() => {
     shareContainer.classList.add("hidden");
+})
+
+btnActiveToggle.addEventListener("click",() => {
+    shareContainer.classList.add("hidden");
+    btnToggle.classList.remove("hidden");
+    btnActiveToggle.classList.add("hidden");
 })
